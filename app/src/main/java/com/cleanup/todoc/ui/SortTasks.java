dc3eecf4e7c07c7d71969b8201d7dffcfefdbdf0
JeanOTTO <124897888+JeanOTTO@users.noreply.github.com>
2023-10-30 16:16:59 +0100

    Create ViewModel Create Enum SortTasks

---
+++ app/src/main/java/com/cleanup/todoc/ui/SortTasks.java
@@ -0,0 +1,24 @@
+package com.cleanup.todoc.ui;
+
+public enum SortTasks {
+    /**
+     * Sort alphabetical by name
+     */
+    ALPHABETICAL,
+    /**
+     * Inverted sort alphabetical by name
+     */
+    ALPHABETICAL_INVERTED,
+    /**
+     * Lastly created first
+     */
+    RECENT_FIRST,
+    /**
+     * First created first
+     */
+    OLD_FIRST,
+    /**
+     * No sort
+     */
+    NONE
+}
---
+++ app/src/main/java/com/cleanup/todoc/ui/TaskViewModel.java
@@ -0,0 +1,130 @@
+package com.cleanup.todoc.ui;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.cleanup.todoc.data.database.entities.Project;
+import com.cleanup.todoc.data.database.entities.Task;
+import com.cleanup.todoc.data.database.entities.TaskWithProject;
+import com.cleanup.todoc.data.repositories.ProjectRepository;
+import com.cleanup.todoc.data.repositories.TaskRepository;
+import com.cleanup.todoc.di.DI;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class TaskViewModel extends AndroidViewModel {
+    private TaskRepository taskRepository;
+    private final ProjectRepository projectRepository;
+    private final Executor executor = Executors.newSingleThreadExecutor();
+    private LiveData<List<Task>> allTasks;
+    private LiveData<List<Project>> allProjects;
+    private MutableLiveData<SortTasks> mutableTaskWithProject = new MutableLiveData<>();
+    //public LiveData<List<TaskWithProject>> taskWithProject = mutableTaskWithProject;
+    public LiveData<List<TaskWithProject>> taskWithProjectObs;
+
+    public TaskViewModel(Application application) {
+        super(application);
+
+        DI.initialize(application.getApplicationContext());
+        taskRepository = DI.getTaskRepository();
+        projectRepository = DI.getProjectRepository();
+        allProjects = projectRepository.getAllProjects();
+        allTasks = taskRepository.getAllTasks();
+        taskWithProjectObs = taskRepository.getAllTasksWithProjects();
+    }
+
+    @NonNull
+    public LiveData<List<TaskWithProject>> getAllTasks() {
+
+        return taskWithProjectObs;
+    }
+
+
+    public LiveData<List<Project>> getAllProjects() {
+        return allProjects;
+    }
+
+    public void insertTask(Task task) {
+        executor.execute(() -> {
+            taskRepository.insertTask(task);
+        });
+    }
+
+    public void deleteTask(TaskWithProject taskWithProject) {
+        executor.execute(() -> {
+            taskRepository.deleteTask(taskWithProject.task);
+        });
+    }
+
+    public List<TaskWithProject> sortTasks(List<TaskWithProject> tasksList, SortTasks mSortTasks) {
+        //List<TaskWithProject> tasks = taskWithProject.getValue();
+        if (tasksList != null) {
+
+            switch (mSortTasks) {
+                case ALPHABETICAL:
+                    Collections.sort(tasksList, new TaskAZComparator());
+                    break;
+                case ALPHABETICAL_INVERTED:
+                    Collections.sort(tasksList, new TaskZAComparator());
+                    break;
+                case RECENT_FIRST:
+                    Collections.sort(tasksList, new TaskOldComparator());
+                    break;
+                case OLD_FIRST:
+                    Collections.sort(tasksList, new TaskRecentComparator());
+                    break;
+        }
+    }
+        return tasksList;
+    }
+
+    /**
+     * Comparator to sort task from A to Z
+     */
+    public static class TaskAZComparator implements Comparator<TaskWithProject> {
+        @Override
+        public int compare(TaskWithProject left, TaskWithProject right) {
+            return left.task.getName().compareToIgnoreCase(right.task.getName());
+        }
+    }
+
+    /**
+     * Comparator to sort task from Z to A
+     */
+    public static class TaskZAComparator implements Comparator<TaskWithProject> {
+        @Override
+        public int compare(TaskWithProject left, TaskWithProject right) {
+            return right.task.getName().compareToIgnoreCase(left.task.getName());
+        }
+    }
+
+    /**
+     * Comparator to sort task from last created to first created
+     */
+    public static class TaskRecentComparator implements Comparator<TaskWithProject> {
+        @Override
+        public int compare(TaskWithProject left, TaskWithProject right) {
+            return left.task.getCreatedAt().compareTo(right.task.getCreatedAt());
+        }
+    }
+
+    /**
+     * Comparator to sort task from first created to last created
+     */
+    public static class TaskOldComparator implements Comparator<TaskWithProject> {
+        @Override
+        public int compare(TaskWithProject left, TaskWithProject right) {
+            return right.task.getCreatedAt().compareTo(left.task.getCreatedAt());
+        }
+    }
+
+
+}
